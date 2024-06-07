a0d2d1402631bad0b1a634af0407c532c20573aa
islammosaad <islammosad@outlook.com>
2024-06-07 21:27:11 +0300

    creating Modes

---
+++ MyServerAPI/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ServerAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int quantity { get; set; }
+        public float price { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public ProductCategory Category { get; set; }
+    }
+
+    public enum ProductCategory
+    {
+        CATEGORY_UNSPECIFIED = 0,
+        LAPTOP = 1,
+        MOBILE = 2,
+        FOOD = 3
+    }
+
+}
---
+++ MyServerAPI/Models/ProductList.cs
@@ -0,0 +1,46 @@
+﻿namespace ServerAPI.Models
+{
+    public static class ProductList
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() {
+                Id = 1,
+                Name = "OPPO",
+                price = 400,
+                quantity = 10,
+                ExpireDate = DateTime.Now.AddMonths(12),
+                Category = ProductCategory.LAPTOP,
+
+
+            },
+            new Product() {
+                Id = 2,
+                Name = "Dell",
+                price = 4000,
+                quantity = 10,
+                ExpireDate = DateTime.Now.AddMonths(12),
+                Category = ProductCategory.LAPTOP
+            },
+
+             new Product() {
+                Id = 3,
+                Name = "HP",
+                price = 4000,
+                quantity = 10,
+                ExpireDate = DateTime.Now.AddMonths(12),
+                Category = ProductCategory.LAPTOP
+            },
+
+             new Product() {
+                Id = 2,
+                Name = "Fish",
+                price = 40,
+                quantity = 100,
+                ExpireDate = DateTime.Now.AddMonths(12),
+                Category = ProductCategory.FOOD
+            },
+
+        };
+    }
+}
