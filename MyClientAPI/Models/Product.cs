6e03cfcf0c2842acf9e7fc16327d0a261411a598
islammosaad <islammosad@outlook.com>
2024-06-07 21:01:44 +0300

    create models

---
+++ MyClientAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace ClientAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int quantity { get; set; }
+        public float price { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public ProductCategory Category { get; set; }
+    }
+
+    public enum ProductCategory
+    {
+        CATEGORY_UNSPECIFIED = 0,
+        LAPTOP = 1,
+        MOBILE = 2,
+        FOOD = 3
+    }
+}
